b299caa0693d4ab128670a8fc4d5cf28eabf62e7
unknown-characteristics <78977619+unknown-characteristics@users.noreply.github.com>
2026-03-17 08:20:41 +0200

    homework 2 (frontend remove StrictMode)

--- hw2/frontend/src/main.jsx
+++ hw2/frontend/src/main.jsx
@@ -5,11 +5,11 @@ import { App } from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
+
 )
